31738715cba7ed74a30c4d5042b03037540b6172
Yangsoo Song <song.yang@husky.neu.edu>
2020-05-23 23:20:56 -0400

    Finished testing controller

--- src/main/java/com/example/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/example/coronavirustracker/controllers/HomeController.java
@@ -2,13 +2,15 @@ package com.example.coronavirustracker.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("testName", "Test");
         return "home";
     }
 
