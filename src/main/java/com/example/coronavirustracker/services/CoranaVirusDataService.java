1ebae2c7206edb849594f582dc32ff1bab0b17b4
Yangsoo Song <song.yang@husky.neu.edu>
2020-05-23 22:39:44 -0400

    Use csv open apache open library to parse data (http://commons.apache.org/proper/commons-csv/user-guide.html)

--- src/main/java/com/example/coronavirustracker/services/CoranaVirusDataService.java
+++ src/main/java/com/example/coronavirustracker/services/CoranaVirusDataService.java
@@ -1,8 +1,11 @@
 package com.example.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -27,6 +30,14 @@ public class CoranaVirusDataService {
             .uri(URI.create(CoronavirusURL))
             .build();
     HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-    System.out.println(httpResponse.body());
+    StringReader csvBodyReader = new StringReader(httpResponse.body());
+    //System.out.println(httpResponse.body());
+    Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+    for (CSVRecord record : records) {
+      String state = record.get("Province/State");
+      System.out.println(state);
+//      String customerNo = record.get("CustomerNo");
+//      String name = record.get("Name");
+    }
   }
 }
