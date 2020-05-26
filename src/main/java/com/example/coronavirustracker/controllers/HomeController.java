215f46f8f0493679b834d609553dabdfaa596b90
Yangsoo Song <song.yang@husky.neu.edu>
2020-05-25 22:52:24 -0400

    Create jumbotron in homepage showing total cases reported as of today with Bootstrap

--- src/main/java/com/example/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/example/coronavirustracker/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package com.example.coronavirustracker.controllers;
 
 
+import com.example.coronavirustracker.models.LocationStats;
 import com.example.coronavirustracker.services.CoronaVirusDataService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -16,7 +19,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("LocationStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats =coronaVirusDataService.getAllStats();
+        int TotalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("LocationStats", allStats);
+        model.addAttribute("TotalReportedCases", TotalReportedCases);
+
         return "home";
     }
 
